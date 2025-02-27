596a476e0877bf2261335f6d19c9c8320445a20a
Emilien CLERJUSTE <e.clerjuste18@yahoo.com>
2025-02-27 10:02:19 +0100

    ylp-mvp todo

--- Gemfile
+++ Gemfile
@@ -46,6 +46,7 @@ group :development, :test do
   gem "debug", platforms: %i[ mri windows ]
 end
 gem "simple_form"
+gem "faker"
 
 group :development do
   # Use console on exceptions pages [https://github.com/rails/web-console]
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,31 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+require 'faker'
+
+# Détruire les anciennes données pour éviter les doublons (optionnel)
+Restaurant.destroy_all
+Review.destroy_all
+
+# Définir les catégories de restaurants
+
+
+# Créer 5 restaurants
+5.times do
+  restaurant = Restaurant.create!(
+    name:         Faker::Restaurant.name,
+    address:      Faker::Address.full_address,
+    phone_number: Faker::PhoneNumber.phone_number,
+    category:     Restaurant::CATEGO.sample
+  )
+
+  # Pour chaque restaurant, créer un nombre aléatoire de reviews (entre 1 et 5 par exemple)
+  # rand(1..5).times do
+  #   restaurant.reviews.create!(
+  #     content: Faker::Restaurant.review,   # Si Faker::Restaurant.review n'existe pas, utilisez Faker::Lorem.sentence
+  #     rating:  rand(0..5)                   # Note aléatoire comprise entre 0 et 5
+  #   )
+  # end
+end
+
+puts "Done!!"
